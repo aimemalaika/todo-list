86f4a848a8a4ef92716ab4a40ecf99824e57ba87
Aime Malaika <aimemalaika1995@gmail.com>
2022-01-14 11:18:11 +0200

    Add crud operations to the todo app

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,7 +6,37 @@ import {
 import '../css/index.css';
 
 (() => {
-  Tasks.load();
+  const count = Tasks.load();
+  document.querySelector('.number-label').innerText = count;
   const refresh = document.querySelector('.refrecher');
   refresh.addEventListener('click', () => refreshPage(refresh));
 })();
+
+const submitData = () => {
+  const description = document.querySelector('#taskname');
+  if (description.value !== '') {
+    const taskObj = new Tasks(description.value, Tasks.db().length);
+    Tasks.addTask(taskObj);
+    description.value = '';
+  }
+};
+
+document.querySelector('#addItem').addEventListener('submit', (e) => {
+  e.preventDefault();
+  submitData();
+});
+
+document.querySelector('.enter-submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  submitData();
+});
+
+document.querySelectorAll('.current-task').forEach((form) => {
+  form.addEventListener('submit', (el) => {
+    el.preventDefault();
+    if (el.target.children[0].value !== '') {
+      Tasks.updateData(el.target.children[0].getAttribute('data-task-id'), el.target.children[0].value);
+      el.target.children[0].blur();
+    }
+  });
+});
